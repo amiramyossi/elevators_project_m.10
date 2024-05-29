0ee2f898ce2c37bb228954890f7c54f0acbe666b
amiramyossi <amiramyossi@gmail.com>
2024-05-29 10:38:47 +0300

    Wrote Level class template

---
+++ Level.py
@@ -0,0 +1,16 @@
+class Level:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def call_lift():
+        pass    
+
+    def display_time():
+        pass
+
+    def ding():
+        pass
+
+    def change_colour():
+        pass    
