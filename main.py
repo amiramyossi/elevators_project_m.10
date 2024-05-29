2d289c56be2e619af2ebc12b0a4bf45efd169359
amiramyossi <amiramyossi@gmail.com>
2024-05-29 10:27:12 +0300

    Wrote building class template

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-print("try git commit and push.")
+class Building:
 
-print("breath")
+    def __init__(self, floor_amount, lift_amount, screen):
+
+        self.__screen = screen 
+
+        self.__floors = []
+        for i in range(floor_amount + 1):
+            self.__floors.append(Floor(i))
+        self.__lifts = []
+        for i in range(lift_amount):
+            self.__lifts.append(Lift(i + 1))
+
+    def run():
+        pass
+
+    def draw_all():
+        pass
+
+    
+
+
+
+
+
+           
+
+      
+
+
+
+
+
+
+        
+
